fa83de03e6dc669c62d3934abba7c645680bf234
rohn <nrohn@tx.rr.com>
2021-01-01 05:53:50 -0600

    Implement all Pickaxe types

--- src/main/java/pepjebs/ruined_equipment/RuinedEquipmentMod.java
+++ src/main/java/pepjebs/ruined_equipment/RuinedEquipmentMod.java
@@ -2,21 +2,54 @@ package pepjebs.ruined_equipment;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pepjebs.ruined_equipment.item.RuinedEquipmentItem;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class RuinedEquipmentMod implements ModInitializer {
 
     public static final String MOD_ID = "ruined_equipment";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
+    public static final String RUINED_PREFIX = "ruined_";
+
     public static final Item RUINED_DIAMOND_PICK = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+    public static final Item RUINED_GOLDEN_PICKAXE = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+    public static final Item RUINED_IRON_PICKAXE = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+    public static final Item RUINED_NETHERITE_PICKAXE = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+    public static final Item RUINED_STONE_PICKAXE = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+    public static final Item RUINED_WOODEN_PICKAXE = new RuinedEquipmentItem(new Item.Settings().maxCount(1));
+
+    public static final Map<Item, Item> VANILLA_ITEM_MAP = new HashMap<Item, Item>() {{
+        put(RUINED_DIAMOND_PICK, Items.DIAMOND_PICKAXE);
+        put(RUINED_GOLDEN_PICKAXE, Items.GOLDEN_PICKAXE);
+        put(RUINED_IRON_PICKAXE, Items.IRON_PICKAXE);
+        put(RUINED_NETHERITE_PICKAXE, Items.NETHERITE_PICKAXE);
+        put(RUINED_STONE_PICKAXE, Items.STONE_PICKAXE);
+        put(RUINED_WOODEN_PICKAXE, Items.WOODEN_PICKAXE);
+    }};
+
+    public static final Map<Item, String> ITEM_ID_MAP = new HashMap<Item, String>() {{
+        put(RUINED_DIAMOND_PICK, "diamond_pickaxe");
+        put(RUINED_GOLDEN_PICKAXE, "golden_pickaxe");
+        put(RUINED_IRON_PICKAXE, "iron_pickaxe");
+        put(RUINED_NETHERITE_PICKAXE, "netherite_pickaxe");
+        put(RUINED_STONE_PICKAXE, "stone_pickaxe");
+        put(RUINED_WOODEN_PICKAXE, "wooden_pickaxe");
+    }};
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "ruined_diamond_pickaxe"), RUINED_DIAMOND_PICK);
+        for (Map.Entry<Item, String> item : ITEM_ID_MAP.entrySet()) {
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID,
+                    RUINED_PREFIX + item.getValue()), item.getKey());
+        }
     }
 }
--- src/main/java/pepjebs/ruined_equipment/mixin/RuinedEquipmentMixin.java
+++ src/main/java/pepjebs/ruined_equipment/mixin/RuinedEquipmentMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
@@ -38,34 +39,36 @@ public class RuinedEquipmentMixin {
             switch(slot) {
                 case MAINHAND:
                     ItemStack breakingToolStack = serverPlayer.getMainHandStack();
-                    if (breakingToolStack.isItemEqualIgnoreDamage(new ItemStack(Items.DIAMOND_PICKAXE))) {
-                        if (breakingToolStack.getMaxDamage() - breakingToolStack.getDamage() == 0) {
-                            ItemStack ruinedPick = new ItemStack(RuinedEquipmentMod.RUINED_DIAMOND_PICK);
-                            Set<String> enchantmentStrings = new HashSet<>();
-                            for (Map.Entry<Enchantment, Integer> ench : EnchantmentHelper.get(breakingToolStack).entrySet()) {
-                                String enchantString = ench.getKey().getName(ench.getValue()).getString();
-                                enchantmentStrings.add(enchantString);
-                                RuinedEquipmentMod.LOGGER.info("sendEquipmentBreakStatus: Adding enchantment: \"" + enchantString + "\"");
-                            }
-                            if (!enchantmentStrings.isEmpty()) {
-                                CompoundTag tag = ruinedPick.getTag();
-                                if (tag == null) tag = new CompoundTag();
-                                tag.putString("enchantments", String.join(",", enchantmentStrings));
-                                ruinedPick.setTag(tag);
-                            }
-                            LiteralText breakingToolName = new LiteralText(breakingToolStack.getName().asString());
-                            if (breakingToolStack.hasCustomName()) {
-                                if (ruinedPick.hasGlint()) {
-                                    ruinedPick.setCustomName(breakingToolName.formatted(Formatting.AQUA)
-                                            .formatted(Formatting.ITALIC));
-                                } else {
-                                    ruinedPick.setCustomName(breakingToolName.formatted(Formatting.WHITE)
-                                            .formatted(Formatting.ITALIC));
+                    for (Map.Entry<Item, Item> itemMap : RuinedEquipmentMod.VANILLA_ITEM_MAP.entrySet()) {
+                        if (breakingToolStack.isItemEqualIgnoreDamage(new ItemStack(itemMap.getValue()))) {
+                            if (breakingToolStack.getMaxDamage() - breakingToolStack.getDamage() == 0) {
+                                ItemStack ruinedPick = new ItemStack(itemMap.getKey());
+                                Set<String> enchantmentStrings = new HashSet<>();
+                                for (Map.Entry<Enchantment, Integer> ench : EnchantmentHelper.get(breakingToolStack).entrySet()) {
+                                    String enchantString = ench.getKey().getName(ench.getValue()).getString();
+                                    enchantmentStrings.add(enchantString);
+                                    RuinedEquipmentMod.LOGGER.info("sendEquipmentBreakStatus: Adding enchantment: \"" + enchantString + "\"");
                                 }
+                                if (!enchantmentStrings.isEmpty()) {
+                                    CompoundTag tag = ruinedPick.getTag();
+                                    if (tag == null) tag = new CompoundTag();
+                                    tag.putString("enchantments", String.join(",", enchantmentStrings));
+                                    ruinedPick.setTag(tag);
+                                }
+                                LiteralText breakingToolName = new LiteralText(breakingToolStack.getName().asString());
+                                if (breakingToolStack.hasCustomName()) {
+                                    if (ruinedPick.hasGlint()) {
+                                        ruinedPick.setCustomName(breakingToolName.formatted(Formatting.AQUA)
+                                                .formatted(Formatting.ITALIC));
+                                    } else {
+                                        ruinedPick.setCustomName(breakingToolName.formatted(Formatting.WHITE)
+                                                .formatted(Formatting.ITALIC));
+                                    }
+                                }
+                                // Set the item in the correct index
+                                serverPlayer.inventory.main.set(serverPlayer.inventory.getSwappableHotbarSlot(),
+                                        ruinedPick);
                             }
-                            // Set the item in the correct index
-                            serverPlayer.inventory.main.set(serverPlayer.inventory.getSwappableHotbarSlot(),
-                                    ruinedPick);
                         }
                     }
                     break;
