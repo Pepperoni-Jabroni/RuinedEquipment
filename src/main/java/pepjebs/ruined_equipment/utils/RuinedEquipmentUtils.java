bf7654ea3f8720290a748a8b32239b4fdb1e1f78
rohn <nrohn@tx.rr.com>
2021-01-02 18:04:23 -0600

    Fix crossbows not being set

--- src/main/java/pepjebs/ruined_equipment/utils/RuinedEquipmentUtils.java
+++ src/main/java/pepjebs/ruined_equipment/utils/RuinedEquipmentUtils.java
@@ -5,6 +5,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.DyeableItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
@@ -25,6 +26,8 @@ public class RuinedEquipmentUtils {
             boolean forceSet) {
         for (Map.Entry<Item, Item> itemMap : RuinedEquipmentItems.VANILLA_ITEM_MAP.entrySet()) {
             if (isVanillaItemStackBreaking(breakingStack, itemMap.getValue())) {
+                // Crossbows don't play nicely for some reason...
+                if (breakingStack.isItemEqualIgnoreDamage(new ItemStack(Items.CROSSBOW))) forceSet = false;
                 ItemStack ruinedStack = new ItemStack(itemMap.getKey());
                 // Set enchantment NBT data
                 CompoundTag enchantTag = getTagForEnchantments(breakingStack, ruinedStack);
@@ -43,7 +46,7 @@ public class RuinedEquipmentUtils {
                     ((DyeableItem) ruinedStack.getItem()).setColor(ruinedStack, breakingColor);
                     DyeableItem.blendAndSetColor(ruinedStack, new LinkedList<>());
                 }
-                // Force set will try & place the Ruined item in hand
+                // Force set will place the Ruined item in hand
                 if (forceSet) {
                     serverPlayer.inventory.setStack(serverPlayer.inventory.selectedSlot, ruinedStack);
                 } else {
