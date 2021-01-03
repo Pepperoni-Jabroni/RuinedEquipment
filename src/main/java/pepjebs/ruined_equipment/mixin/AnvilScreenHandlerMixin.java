d24abadcac77e37aa87b4fd4093bfe8167d134a7
rohn <nrohn@tx.rr.com>
2021-01-03 00:37:38 -0600

    Remove for loop iterating over ruined items

--- src/main/java/pepjebs/ruined_equipment/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/pepjebs/ruined_equipment/mixin/AnvilScreenHandlerMixin.java
@@ -18,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pepjebs.ruined_equipment.RuinedEquipmentMod;
+import pepjebs.ruined_equipment.item.RuinedEquipmentItem;
 import pepjebs.ruined_equipment.item.RuinedEquipmentItems;
 import pepjebs.ruined_equipment.recipe.RuinedEquipmentCraftRepair;
 
@@ -25,6 +26,8 @@ import java.util.Map;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
+
+    private static final double REPAIR_MODIFIER = 0.18;
 
     @Shadow
     @Final
@@ -38,31 +41,30 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
     private void onUpdateResult(CallbackInfo ci) {
         ItemStack leftStack = this.input.getStack(0);
         ItemStack rightStack = this.input.getStack(1);
-        for (Map.Entry<Item, Item> item : RuinedEquipmentItems.VANILLA_ITEM_MAP.entrySet()) {
-            if (leftStack.getItem() == item.getKey()) {
-                // Check right stack for matching repair item
-                if(item.getValue() instanceof ArmorItem) {
-                    ArmorItem armor = (ArmorItem) item.getValue();
-                } else if (item.getValue() instanceof ToolItem) {
-                    ToolItem tool = (ToolItem) item.getValue();
-                    Ingredient ing = tool.getMaterial().getRepairIngredient();
-                    if (ing.test(rightStack)) {
-                        ItemStack repaired = new ItemStack(item.getValue());
-                        int targetDamage = (int) ((1.0 - (rightStack.getCount() * RuinedEquipmentCraftRepair.REPAIR_MODIFIER)) * repaired.getMaxDamage());
-                        repaired.setDamage(targetDamage);
-                        if (leftStack.hasCustomName()) {
-                            repaired.setCustomName(leftStack.getName());
-                        }
-                        this.output.setStack(0, repaired);
-                        this.sendContentUpdates();
-                    }
+        if (leftStack.getItem() instanceof RuinedEquipmentItem) {
+            RuinedEquipmentItem ruinedItem = (RuinedEquipmentItem) leftStack.getItem();
+            Item vanillaItem = RuinedEquipmentItems.VANILLA_ITEM_MAP.get(ruinedItem);
+            // Check right stack for matching repair item
+            Ingredient repairIngredient = null;
+            if(vanillaItem instanceof ArmorItem) {
+                repairIngredient = ((ArmorItem) vanillaItem).getMaterial().getRepairIngredient();
+            } else if (vanillaItem instanceof ToolItem) {
+                repairIngredient = ((ToolItem) vanillaItem).getMaterial().getRepairIngredient();
+            }
+            if (repairIngredient != null && repairIngredient.test(rightStack)) {
+                ItemStack repaired = new ItemStack(vanillaItem);
+                int targetDamage = (int) ((1.0 - (rightStack.getCount() * REPAIR_MODIFIER)) * repaired.getMaxDamage());
+                repaired.setDamage(targetDamage);
+                if (leftStack.hasCustomName()) {
+                    repaired.setCustomName(leftStack.getName());
                 }
-                // Check right stack for corresponding vanilla item
-                if (rightStack.getItem() == item.getValue()) {
+                this.output.setStack(0, repaired);
+                this.sendContentUpdates();
+                return;
+            }
+            // Check right stack for corresponding vanilla item
+            if (rightStack.getItem() == vanillaItem) {
 
-                }
-                // Discontinue loop
-                break;
             }
         }
     }
